ca824871388a756b34cd5e1db35cc153c719aedb
Matheus Zabin <matheusqueirozzabin@gmail.com>
2021-10-31 19:56:34 -0300

    implement signature test

--- pkg/jwt/sign_test.go
+++ pkg/jwt/sign_test.go
@@ -1,1 +1,19 @@
-package jwt_test
+package jwt
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetSignature(t *testing.T) {
+	require := require.New(t)
+
+	// payload is base64 for `{"lala":"lala","lele":"lele"}`
+	payload := "eyJsYWxhIjoibGFsYSIsImxlbGUiOiJsZWxlIn0"
+	secret := "lalalala"
+
+	// This value is retrieve from online HMAC SHA256 and base64 encoders
+	signature := getSignature(secret, payload)
+	require.Equal("MTE0YmY1Y2YzZmMyMGU1YWJhMTgxODQ3ZTMwOGEyMDQ2N2YwYTM5YmYzZjhlMzNjMTMxYjVhMDhiZTgxNWEyMA==", signature)
+}
